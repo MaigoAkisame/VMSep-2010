63e20218e67f09089816cdaf8cc3c59ea171b15c
Ming <mingdu2015@u.northwestern.edu>
2016-11-29 00:46:04 -0600

    sample rate converter

--- code/sr_convert.m
+++ code/sr_convert.m
@@ -1,5 +1,11 @@
-raw_sr = 44100;
-input = audioread('mono_jp.wav');
+function sr_convert(fname, fname_out)
+
+% convert a 2-channel sound file with arbitrary sample rate to 16 kHz
+% single channel.
+[dat,raw_sr] = audioread(fname);
+dat = (dat(:, 1)+dat(:, 2))/2;
 [P,Q] = rat(16000/raw_sr);
-input = resample(input, P, Q);
-audiowrite('mono16_jp.wav', input, 16000);
+dat = resample(dat, P, Q);
+audiowrite(fname_out, dat, 16000);
+
+end
